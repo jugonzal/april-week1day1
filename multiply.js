18f404dfcf270ea2b89089304b91511646b69492
Juan <juan@videogami.co>
2017-04-03 16:11:43 -0400

    Taken two numbers and multiply.  Done

--- multiply.js
+++ multiply.js
@@ -3,6 +3,18 @@
 // output: those same two numbers multiplied
 // any tricks?  how to get the numbers from the command line ?
 
-console.log("Hello!");
+console.log("Hello! I will multiply your numbers...");
 
-console.log(process.argv);
+// console.log(process.argv);
+
+// get the two numbers
+console.log(process.argv[2]);
+console.log(process.argv[3]);
+
+var number1 = process.argv[2];
+var number2 = process.argv[3];
+
+// I should multiply them
+// show the results
+console.log(number1 * number2);
+
