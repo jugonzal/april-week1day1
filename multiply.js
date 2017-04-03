a743804e5c072066940a20a6e2bd2cb9521dcf8c
Juan <juan@videogami.co>
2017-04-03 16:02:26 -0400

    Figured out how to read arguments from command line.. and say hello

---
+++ multiply.js
@@ -0,0 +1,8 @@
+// a program
+// input: two numbers
+// output: those same two numbers multiplied
+// any tricks?  how to get the numbers from the command line ?
+
+console.log("Hello!");
+
+console.log(process.argv);
